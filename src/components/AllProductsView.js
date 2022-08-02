48dc8ac38294cc8883b8b9dfc46facae28d3a896
ymei0072 <yukstormei@gmail.com>
2022-08-02 15:09:08 -0400

    AllProductsView component page - in progress

---
+++ src/components/AllProductsView.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+//import { connect } from 'react-redux';
+
+export class AllProductsView extends Component {
+  // componentDidMount() {
+  //     this.props.getProducts();
+  //   }
+
+  render() {
+    //const { products } = this.props
+    return (
+      <div>
+        {products.map((product) => {
+          <div key={product.id}>
+            <img src={product.imageUrl} width="200" height="150" />
+            <div>{product.name}</div>
+            <div>{product.price}</div>
+            <div>{product.description}</div>
+            <div>{product.review}</div>
+          </div>;
+        })}
+      </div>
+    );
+  }
+}
+
+// const mapState = ({ products }) => ({
+//     products,
+//   });
+
+//   const mapDispatch = (dispatch) => ({
+//     getProducts: () => dispatch(fetchProducts()),
+//   });
+
+//   export default connect(mapState, mapDispatch)(AllProductsView);
