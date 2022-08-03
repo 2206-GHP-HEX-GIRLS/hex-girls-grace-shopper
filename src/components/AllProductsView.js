505864e851bf53b3040bc332f942f5154eda2d4c
dakotafabro <dakotafabrocgu@gmail.com>
2022-08-03 07:41:49 -0700

    implemented functional hooks

--- src/components/AllProductsView.js
+++ src/components/AllProductsView.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchProducts } from '../reducers/products';
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { fetchProducts } from "../reducers/products";
 
-export class AllProductsView extends Component {
-  componentDidMount() {
-    this.props.getProducts();
-  }
+const AllProductsView = (props) => {
+  let [products, setProducts] = useState(props.products ? props.products : "");
 
-  render() {
-    const { products } = this.props;
-    return (
-      <div>
-        {products.map((product) => (
-          <div key={product.id}>
-            <img src={product.imageUrl} alt="baked goods img" />
-            <div>{product.name}</div>
-            <div>{product.price}</div>
-            <div>{product.description}</div>
-            <div>{product.review}</div>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    props.getProducts();
+  }, []);
+
+  useEffect(() => {
+    props.getProducts();
+  }, [products]);
+
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>
+          <img src={product.imageUrl} alt="baked goods img" />
+          <div>{product.name}</div>
+          <div>{product.price}</div>
+          <div>{product.description}</div>
+          <div>{product.review}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const mapState = ({ products }) => ({
   products,
