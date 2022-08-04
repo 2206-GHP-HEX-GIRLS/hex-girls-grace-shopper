204cb00e6f5c5d478a412428a07be2315ed841a9
Eve Frances <evefrances92@icloud.com>
2022-08-04 17:04:06 -0400

    added require to product routes, added cart reducer file, added cart route

---
+++ src/server/api/cart.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const Cart = require('../db/models/lineitem')
+
+//display cart
+router.get('/:id', async (req, res, next) => {
+    try{
+        const cart = await Cart.findByPk(req.params.id)
+    res.json(cart);
+} catch (error) {
+    next(error);
+}
+})
+
+//delete item from cart
+
+//add item to cart?
+//^^ this should just go back to the shop. most likely a
+//component route/frontend route.
+
+//update item in cart?
