7d3670566a7bc03880aa20201018cf034d39f926
ymei0072 <yukstormei@gmail.com>
2022-08-05 12:26:18 -0400

    process.env.DATABASE_URL for heroku

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -2,9 +2,7 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize(
   process.env.DATABASE_URL || 'postgres://localhost:5432/bakery',
-  // {
-  //   logging: false,
-  // }
+  // {logging: false}
   {
     dialectOptions: {
       ssl: {
