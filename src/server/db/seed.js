858a173f009a456b614e6f1e00edd85b0403b87d
dakotafabro <dakotafabrocgu@gmail.com>
2022-08-05 08:47:09 -0700

    updated createLogger and croissant image in seed

--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -1,69 +1,68 @@
-const { db, Product, User, Review, Order, LineItem } = require('../db');
+const { db, Product, User, Review, Order, LineItem } = require("../db");
 
 const products = [
   {
-    name: 'Chocolate chip cookie',
+    name: "Chocolate chip cookie",
     price: 1.5,
-    imageUrl: 'https://cdn-icons-png.flaticon.com/512/541/541732.png',
-    description: 'Yummy',
+    imageUrl: "https://cdn-icons-png.flaticon.com/512/541/541732.png",
+    description: "Yummy",
     quantity: 1,
-    category: 'Cookies',
+    category: "Cookies",
   },
   {
-    name: 'Galaxy chip cookie',
+    name: "Galaxy chip cookie",
     price: 2.5,
-    imageUrl: 'https://cdn-icons-png.flaticon.com/512/164/164659.png',
-    description: 'Out of this world',
+    imageUrl: "https://cdn-icons-png.flaticon.com/512/164/164659.png",
+    description: "Out of this world",
     quantity: 1,
-    category: 'Cookies',
+    category: "Cookies",
   },
   {
-    name: 'Strawberry short cake',
+    name: "Strawberry short cake",
     price: 10.0,
-    imageUrl: 'https://cdn-icons-png.flaticon.com/512/992/992754.png',
-    description: 'Short',
+    imageUrl: "https://cdn-icons-png.flaticon.com/512/992/992754.png",
+    description: "Short",
     quantity: 1,
-    category: 'Cakes',
+    category: "Cakes",
   },
   {
-    name: 'croissant',
+    name: "croissant",
     price: 2.0,
-    imageUrl:
-      'https://cdn-icons.flaticon.com/png/512/3070/premium/3070562.png?token=exp=1659642826~hmac=61051052803ac5969da1d7b48fc83933',
-    description: 'bent',
+    imageUrl: "https://i.imgur.com/etqhIPo.png",
+    description: "bent",
     quantity: 1,
-    category: 'Pastries',
+    category: "Pastries",
   },
 ];
 
 const users = [
   {
-    username: 'Cookie Monster',
-    password: '123',
-    email: 'sweettooth@gmail.com',
+    username: "Cookie Monster",
+    password: "123",
+    email: "sweettooth@gmail.com",
   },
   {
-    username: 'Bent Croissant',
-    password: '456',
-    email: 'flakeyfullstack@gmail.com',
+    username: "Bent Croissant",
+    password: "456",
+    email: "flakeyfullstack@gmail.com",
   },
 ];
 
 const reviews = [
   {
-    content: 'wow what a great croissant',
+    content: "wow what a great croissant",
     rating: 5,
     userId: 1,
     productId: 4,
   },
   {
-    content: 'wow what a great cake',
+    content: "wow what a great cake",
     rating: 2,
     userId: 2,
     productId: 3,
   },
   {
-    content: 'wow what a yummy cookie',
+    content: "wow what a yummy cookie",
     rating: 2,
     userId: 1,
     productId: 2,
@@ -89,11 +88,11 @@ const seed = async () => {
         return Review.create(review);
       })
     );
-    console.log('db synced');
+    console.log("db synced");
     db.close();
   } catch (error) {
     console.error(error);
-    console.error('Failed to seed! :(');
+    console.error("Failed to seed! :(");
     db.close();
   }
 };
