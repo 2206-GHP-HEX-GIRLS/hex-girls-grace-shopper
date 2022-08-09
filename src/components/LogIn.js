1a2dc63536d85b2c390615befd5c6bef42c7a146
ymei0072 <yukstormei@gmail.com>
2022-08-09 15:48:52 -0400

    firebase not working

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,7 +1,30 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
 import './css/LogIn.css';
-// import { GoogleLogin } from 'react-google-login';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+// initializeApp({
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJ_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STOR_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDR_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// });
+initializeApp({
+  apiKey: 'AIzaSyAXsNmNFLAlhe9llgpU7lkayqvt4yuPhb0',
+  authDomain: 'booleanbakers.firebaseapp.com',
+  projectId: 'booleanbakers',
+  storageBucket: 'booleanbakers.appspot.com',
+  messagingSenderId: '792285495931',
+  appId: '1:792285495931:web:281671c3184a6efee398de',
+});
+
+// const auth = getAuth(app);
+const auth = getAuth();
 
 const LogIn = () => {
   const userRef = useRef();
@@ -20,10 +43,13 @@ const LogIn = () => {
     setErrMsg('');
   }, [user, pwd]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(provider);
+  };
 
-    // setAuth({ user, pwd, roles, accessToken });
+  const handleSubmit = (e) => {
+    e.preventDefault();
     try {
       setUser('');
       setPwd('');
@@ -79,6 +105,9 @@ const LogIn = () => {
             />
             <button>Sign In</button>
           </form>
+
+          <button onClick={signInWithGoogle}>Sign in with Google</button>
+
           <p>
             Need an Account?
             <br />
@@ -91,34 +120,5 @@ const LogIn = () => {
     </div>
   );
 };
-
-// // const clientId = process.env.GOOGLE_LOGIN_CLIENT_ID;
-// //NOT CALLING CLIENT ID PROPERLY WILL NEED TO DETERMINE WHY OR IF IT EVEN NEEDS TO BE HIDDEN
-
-// const clientId =
-//   '792285495931-75b5p0n3tv076s1rq5q69cl9q9pnlvol.apps.googleusercontent.com';
-
-// const LogIn = () => {
-//   const onLoginSuccess = (res) => {
-//     console.log('Login Success:', res.profileObj);
-//   };
-
-//   const onLoginFailure = (res) => {
-//     console.log('Login Failed:', res);
-//   };
-
-//   return (
-//     <div>
-//       <GoogleLogin
-//         clientId={clientId}
-//         buttonText="Sign In"
-//         onSuccess={onLoginSuccess}
-//         onFailure={onLoginFailure}
-//         cookiePolicy={'single_host_origin'}
-//         isSignedIn={true}
-//       />
-//     </div>
-//   );
-// };
 
 export default LogIn;
