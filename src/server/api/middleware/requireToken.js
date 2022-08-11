971693b66d27fdd4c6feaa498b883f5a17df3116
dakotafabro <dakotafabrocgu@gmail.com>
2022-08-11 01:12:58 -0700

    completed requireToken

--- src/server/api/middleware/requireToken.js
+++ src/server/api/middleware/requireToken.js
@@ -2,11 +2,23 @@ const getAuth = require("firebase/auth");
 const User = require("../../db/models/user");
 
 const requireToken = async (req, res, next) => {
-  // const token = req.headers.authorization;
+  const token = req.headers.authorization;
   try {
-    // const user = await User;
-    // req.user = user;
-    next();
+    const user = await getAuth().getUser(token);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      try {
+        const user = await User.byToken(token);
+        if (user) {
+          req.user = user;
+          next();
+        }
+      } catch (error) {
+        next(error);
+      }
+    }
   } catch (error) {
     next(error);
   }
